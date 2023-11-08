07d60728b24bd8cad4a3997c41aa0b1ab3e16b85
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2023-11-08 22:11:03 +0530

    init commit

---
+++ lib/game_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatefulWidget {
+  String player1;
+  String player2;
+
+  GameScreen({required this.player1, required this.player2});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+//game screen.
